0c0d5a86592defde89cb28e0287752759516e5eb
divaharsha <divakarvan03@gmail.com>
2023-01-13 10:34:22 +0530

    settings api updated

--- api/settings.php
+++ api/settings.php
@@ -9,15 +9,18 @@ header("Pragma: no-cache");
 
 include_once('../includes/crud.php');
 date_default_timezone_set('Asia/Kolkata');
+include_once('../includes/functions.php');
+$fn = new functions;
+$fn->monitorApi('settings');
 $db = new Database();
 $db->connect();
-$sql = "SELECT * FROM `notifications` ORDER BY id DESC LIMIT 20 ";
+$sql = "SELECT chat_support FROM `settings`";
 $db->sql($sql);
 $res = $db->getResult();
 $num = $db->numRows($res);
 if ($num >= 1) {
     $response['success'] = true;
-    $response['message'] = "Notification listed Successfully";
+    $response['message'] = "Settings listed Successfully";
     $response['data'] = $res;
     print_r(json_encode($response));
 
