baecec4928ef7bdecc8f010601d78e0c7b6bef60
jayaprasad356 <jayaprasad356@gmail.com>
2023-01-16 10:09:59 +0530

    crud

--- includes/crud.php
+++ includes/crud.php
@@ -13,9 +13,9 @@ class Database
 
 
     private $db_host = "localhost";
-    private $db_user = "u743445510_fortune";
-    private $db_pass = "Fortune@2023"; 
-    private $db_name = "u743445510_fortune"; 
+    private $db_user = "u152146582_fa";
+    private $db_pass = "Fapp@2022"; 
+    private $db_name = "u152146582_fa"; 
 
     /*
      * Extra variables that are required by other function such as boolean con variable
