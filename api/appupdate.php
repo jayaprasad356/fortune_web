acfec169960c80fc45ea1855852983e9178f971f
jayaprasad356 <jayaprasad356@gmail.com>
2023-08-29 12:47:15 +0530

    appupdate

--- api/appupdate.php
+++ api/appupdate.php
@@ -29,10 +29,12 @@ $code_set = $db->getResult();
 $min_codes=$code_set[0]['min_codes'];
 $min_days=$code_set[0]['min_days'];
 $code_gererate_time=$code_set[0]['code_generate_time'];
-
+$sql = "SELECT * FROM app_settings";
+$db->sql($sql);
+$appres = $db->getResult();
 $res = array();
 if($user_id != ''){
-    $sql = "SELECT code_generate_time,total_referrals,withdrawal,last_updated,device_id,datediff('$date', joined_date) AS history_days,datediff('$datetime', last_updated) AS days,code_generate,withdrawal_status,status,joined_date,today_codes,trial_expired,task_type,champion_task_eligible,trial_count,mcg_timer,security,ongoing_sa_balance,salary_advance_balance,sa_refer_count,level,per_code_val  FROM users WHERE id = $user_id ";
+    $sql = "SELECT black_box,code_generate_time,total_referrals,withdrawal,last_updated,device_id,datediff('$date', joined_date) AS history_days,datediff('$datetime', last_updated) AS days,code_generate,withdrawal_status,status,joined_date,today_codes,trial_expired,task_type,champion_task_eligible,trial_count,mcg_timer,security,ongoing_sa_balance,salary_advance_balance,sa_refer_count,level,per_code_val  FROM users WHERE id = $user_id ";
     $db->sql($sql);
     $res = $db->getResult();
     $history_days = $res[0]['history_days'];
@@ -43,6 +45,10 @@ if($user_id != ''){
     $user_code_generate_time = $res[0]['code_generate_time'];
    
     $champion_task = $set[0]['champion_task'];
+    if($res[0]['black_box'] == 1 ){
+        $appres[0]['version'] = '13';
+
+    }
 
 
     $sql = "UPDATE `users` SET  `app_version` = $app_version WHERE `id` = $user_id";
@@ -90,9 +96,7 @@ if($user_id != ''){
 
     }
 }
-$sql = "SELECT * FROM app_settings";
-$db->sql($sql);
-$appres = $db->getResult();
+
 $response['success'] = true;
 $response['message'] = "App Update listed Successfully";
 $response['data'] = $appres;
