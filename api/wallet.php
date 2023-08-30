6a0f74dc849b4bc10e87d3d7e7c5e26a58c2aad4
jayaprasad356 <jayaprasad356@gmail.com>
2023-08-30 16:38:06 +0530

    wallet

--- api/wallet.php
+++ api/wallet.php
@@ -30,6 +30,7 @@ if (empty($_POST['user_id'])) {
 $user_id = $db->escapeString($_POST['user_id']);
 $function->monitorUserApi('wallet',$user_id);
 $codes = (isset($_POST['codes']) && $_POST['codes'] != "") ? $db->escapeString($_POST['codes']) : 0;
+$user_black_box = (isset($_POST['black_box']) && $_POST['black_box'] != "") ? $db->escapeString($_POST['black_box']) : '0';
 $datetime = date('Y-m-d H:i:s');
 
 $type = 'generate';
@@ -43,6 +44,26 @@ $set = $db->getResult();
 $code_generate = $set[0]['code_generate'];
 $sync_codes = $set[0]['sync_codes'];
 $code_min_sync_time = $fn->get_sync_time($ures[0]['level']);
+
+if($user_black_box == '1'){
+    $sql = "SELECT * FROM `suspect_codes` WHERE user_id = $user_id AND status = 0 ORDER BY id DESC LIMIT 1";
+    $db->sql($sql);
+    $res = $db->getResult();
+    $num = $db->numRows($res);
+    if ($num == 1) {
+        $s_id = $res[0]['id'];
+        $sql = "UPDATE `suspect_codes` SET  `status` = 1 WHERE `id` = $s_id";
+        $db->sql($sql);
+        
+    }else{
+        $response['success'] = false;
+        $response['message'] = "You cannot Sync now,please contact admin";
+        print_r(json_encode($response));
+        return false;
+
+    }
+
+}
 $sql = "SELECT datetime FROM transactions WHERE user_id = $user_id AND type = 'generate' ORDER BY datetime DESC LIMIT 1 ";
 $db->sql($sql);
 $tres = $db->getResult();
