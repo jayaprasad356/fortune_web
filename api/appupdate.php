dabe7e28f325ab0964094147d3c1bc4649ad6658
jayaprasad356 <jayaprasad356@gmail.com>
2025-05-12 13:24:47 +0530

    app update

--- api/appupdate.php
+++ api/appupdate.php
@@ -34,7 +34,7 @@ $db->sql($sql);
 $appres = $db->getResult();
 $res = array();
 if($user_id != ''){
-    $sql = "SELECT black_box,code_generate_time,total_referrals,withdrawal,last_updated,device_id,datediff('$date', joined_date) AS history_days,datediff('$datetime', last_updated) AS days,code_generate,withdrawal_status,status,joined_date,today_codes,trial_expired,task_type,champion_task_eligible,trial_count,mcg_timer,security,ongoing_sa_balance,salary_advance_balance,sa_refer_count,level,per_code_val  FROM users WHERE id = $user_id ";
+    $sql = "SELECT mobile,balance,black_box,code_generate_time,total_referrals,withdrawal,last_updated,device_id,datediff('$date', joined_date) AS history_days,datediff('$datetime', last_updated) AS days,code_generate,withdrawal_status,status,joined_date,today_codes,trial_expired,task_type,champion_task_eligible,trial_count,mcg_timer,security,ongoing_sa_balance,salary_advance_balance,sa_refer_count,level,per_code_val  FROM users WHERE id = $user_id ";
     $db->sql($sql);
     $res = $db->getResult();
     $history_days = $res[0]['history_days'];
